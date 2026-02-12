68240ff34d159b8216daf409f47aeed62a72ae5a
FreakTengen <132749880+FreakTengen@users.noreply.github.com>
2026-02-12 08:46:40 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+var licznik = 1
+var clicked = {}
+
+function generuj(id){
+    if
+})
+
+
+
+
+int = licznik
+if (licznik % 2 == 0){
+    document.getElementById("one").innerHTML = "X"
+} else {
+    document.getElementById("one").innerHTML = "O"
+}
